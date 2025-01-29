c4315b931b746e268037e114d43c6055014e7448
AndrosRC <octaviodejesusruizcontreras0@gmail.com>
2025-01-28 22:23:50 -0600

    Primer commit - Subiendo el proyecto

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonButton, IonItem, IonLabel, IonInput } from '@ionic/angular/standalone';
 import { ExploreContainerComponent } from '../explore-container/explore-container.component';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent],
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent, IonButton, IonItem, IonLabel, IonInput],
 })
 export class Tab1Page {
+  
   constructor() {}
 }
--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,13 +1,37 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
-import { ExploreContainerComponent } from '../explore-container/explore-container.component';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonItem, IonLabel, IonToggle, IonSelect, IonSelectOption, IonIcon, IonButton, IonAlert } from '@ionic/angular/standalone';
+import { addIcons } from 'ionicons';
+import { logOutOutline } from 'ionicons/icons';
+import type { OverlayEventDetail } from '@ionic/core';
 
 @Component({
   selector: 'app-tab3',
   templateUrl: 'tab3.page.html',
   styleUrls: ['tab3.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent],
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonItem, IonLabel, IonToggle, IonSelect, IonSelectOption, IonIcon, IonButton, IonAlert], 
 })
 export class Tab3Page {
-  constructor() {}
+  public alertButtons = [
+    {
+      text: 'Más Tarde',
+      role: 'cancel',
+      handler: () => {
+        console.log('Alerta cancelada');
+      },
+    },
+    {
+      text: 'Calificar',
+      role: 'confirm',
+      handler: () => {
+        console.log('Alerta confirmada');
+      },
+    },
+  ];
+
+  setResult(event: CustomEvent<OverlayEventDetail>) {
+    console.log(`Alerta cerrada con el resultado: ${event.detail.role}`);
+  }
+  constructor() {
+    addIcons({ logOutOutline });
+  }
 }
